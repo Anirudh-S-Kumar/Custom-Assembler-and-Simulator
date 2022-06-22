a64a91e32a2cc667fe6330e3c3677a59e1ff7876
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-06-22 20:16:00 +0530

    Added isEmpty separately

--- Assembler/validity_checker.py
+++ Assembler/validity_checker.py
@@ -12,10 +12,18 @@ def isRegister(val: str) -> bool:
         return True
     return False
 
-def ValidityChecker(inst: str) -> bool:
+def isEmpty(inst: str) -> bool:
+    if not inst: # ignoring empty lines
+        return True    
+
+def ValidityCheckerInstr(inst: str) -> bool:
     """return True if the instruction is a valid instruction, else returns false"""
     #Convert the instruction into 
-    if not inst: # ignoring empty lines
-        return True
     
     inst.split()
+
+    if not (isInstruction(inst[0])): # Ignoring labels
+        return False
+
+    type = opcode[inst[0]]["type"]
+    type_struct = type_structure[type]
