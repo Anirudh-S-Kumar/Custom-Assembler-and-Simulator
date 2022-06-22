13a7917bb75878636544f51ad6cdf26f237efaec
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-06-22 17:33:22 +0530

    Converted json data to python usable data

--- Assembler/all_constants.py
+++ Assembler/all_constants.py
@@ -4,7 +4,22 @@ will be extracted and stored in python for ease of access"""
 import json
 import pprint
 
-inst = {}
+
+# Defining the dictionaries
+inst = terms = no_of_register = {}
+unused_bits = memory_bits = {}
+immediate_values = register_addr = {}
+
+
+#Extracting data from them
 with open("ISA_Instructions.json") as f:
     inst = json.load(f)
+with open("other_constants.json") as f:
+    temp = json.load(f)
 
+    terms = f[terms]
+    no_of_register = f[no_of_register]
+    unused_bits = f[unused_bits]
+    memory_bits = f[memory_bits]
+    immediate_values = f[immediate_values]
+    register_addr = f[register_addr]
