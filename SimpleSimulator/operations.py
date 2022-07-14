858b39820b5da5bbe3401b352b7c07314b24efc5
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-07-14 16:34:37 +0530

    added more helpers, half completed add operation

--- SimpleSimulator/operations.py
+++ SimpleSimulator/operations.py
@@ -1,11 +1,17 @@
-import simulator_helpers
+from simulator_helpers import getRegValue, setRegValue
 
 # 10110 00 011 001 010
 
+# Make sure to raise flags incase of underflow and overflow
+
+
 def add(inst):
     print(inst[7:10])
-    reg1_value = simulator_helpers.getRegValue(inst[7:10])
+    reg2_value = getRegValue(inst[10:13])
+    reg3_value = getRegValue(inst[13:])
 
+    reg1_value = reg2_value + reg3_value
+    setRegValue(reg1_value, inst[7:10])
 
 
 def main():
--- SimpleSimulator/simulator_helpers.py
+++ SimpleSimulator/simulator_helpers.py
@@ -18,6 +18,9 @@ def setRegValue(value: int, address: str) -> None:
     key = list(internalDict.keys())[0]
     internalDict[key] = value
 
+def overflow(value: int) -> bool:
+    """Returns true if value is more than 255, or less than 0"""
+    return -1 < value < 256
 
 def main():
     pass
