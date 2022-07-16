d40210a2ab5257a3adb5bcb4a6657f656913c317
RUCHI21CODES <97395218+RUCHI21CODES@users.noreply.github.com>
2022-07-16 18:55:46 +0530

    Update generateBinary.py
    Modified type b instructions so that it can sync with float type instructions as well

--- Simple-Assembler/generateBinary.py
+++ Simple-Assembler/generateBinary.py
@@ -1,8 +1,8 @@
-from allConstants import *
-from assemblerHelpers import returnType
+from all_constants import *
+from assembler_helpers import returnType
 
 
-def typeA(ins:str) -> str:
+def type_a(ins:str) -> str:
     ins = ins.split()
     op = opcodes["type-a"][ins[0]]
     
@@ -12,15 +12,27 @@ def typeA(ins:str) -> str:
 
     return (op + "00" + r1 + r2 + r3)
 
-def typeB(ins:str) ->str:
+    '''def type_b(ins:str) ->str:  #modifying type b instruction as immediate value is present here only
+        ins = ins.split()
+        op = opcodes["type-b"][ins[0]]
+        r1 = register_addr[ins[1]]
+        
+        imm = '{0:08b}'.format(int(ins[2][1:]))
+        return (op+r1+imm)'''
+def type_b(ins:str) ->str:  #modifying type b instruction as immediate value is present here only
     ins = ins.split()
     op = opcodes["type-b"][ins[0]]
     r1 = register_addr[ins[1]]
-    imm = '{0:08b}'.format(int(ins[2][1:]))
+    if ValidInst==True and instMessage == 1:  #validinstr and instmessage is defined in validity checker
+        #int(instToken[i][1:]) should go in mov
+        imm = '{0:08b}'.format(int(ins[2][1:]))
+    if ValidInst==True and instMessage == 0:
+        imm = '{0:08b}'.format(float(ins[2][1:]))
     return (op+r1+imm)
 
 
-def typeC(ins:str) ->str:
+
+def type_c(ins:str) ->str:
     ins = ins.split()
     op = opcodes["type-c"][ins[0]]
     r1 = register_addr[ins[1]]
@@ -28,7 +40,7 @@ def typeC(ins:str) ->str:
     return (op+"00000"+r1+r2)  #since there's no sample test case for this statement so I have not checked
 
 
-def typeD(ins:str, variables:list)->str:
+def type_d(ins:str, variables:list)->str:
     ins = ins.split()
     #define a key
     a_key = ins[2]
@@ -42,27 +54,29 @@ def typeD(ins:str, variables:list)->str:
     #addr = (ins[2])
     return op+r1+mem_addr
 
-def typeE(ins:str, memory:dict)->str:  #no test cases given in the assignment so, I assume its okay only, check once with edge cases
+def type_e(ins:str, memory:dict)->str:  #no test cases given in the assignment so, I assume its okay only, check once with edge cases
     ins = ins.split()
     addr = memory[ins[1]]
     mem_addr = '{0:08b}'.format(int(addr))
     op = opcodes["type-e"][ins[0]]
     return op+"000"+mem_addr  
 
-def typeF(ins:str) ->str:
+def type_f(ins:str) ->str:
     ins = ins.split()
     op = opcodes["type-f"][ins[0]]
     return (op+"00000000000")
 
+
+
 def returnBinary(inst:str, variables:list, memory:dict) -> str:
     """Returns the final binary code to be written in the file"""
     type = returnType(inst)
-    if type == "a":     return typeA(inst)
-    if type == "b":     return typeB(inst)
-    if type == "c":     return typeC(inst)
-    if type == "d":     return typeD(inst, variables)
-    if type == "e":     return typeE(inst, memory)
-    if type == "f":     return typeF(inst)
+    if type == "a":     return type_a(inst)
+    if type == "b":     return type_b(inst)
+    if type == "c":     return type_c(inst)
+    if type == "d":     return type_d(inst, variables)
+    if type == "e":     return type_e(inst, memory)
+    if type == "f":     return type_f(inst)
 
 if __name__ == "__main__":
     print(returnBinary("ld R2 abc",[{"xyz": 8, "abc": 9}], {}))
