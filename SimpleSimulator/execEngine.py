17e904d965b3ad9e268cc486d91d3660fb6ee220
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-07-18 12:52:42 +0530

    Revert "added more helper functions, completed mov"
    This reverts commit 08dbbb1cc64522af3a35e06b85dc0aa4bbad7fb2.

--- SimpleSimulator/execEngine.py
+++ SimpleSimulator/execEngine.py
@@ -1,25 +1,15 @@
 import operations
-from simulatorHelpers import getRegValue
 
 
 
 
 
 
-def execEngine(inst: str, pc:int) -> tuple:
+
+def execEngine(inst: str) -> tuple:
     """
     Will perform all the computations necessary for the given instructions, and return a tuple
     First value will be if the program has to be halted or not
     Second value will be new value of program counter
     """
-    opcode = inst[:5]
-    a1 = tuple(map(operations.mapping[opcode], [inst], [pc]))
-    print(a1)
-    
-
-
-def main():
-    execEngine("")
-
-if __name__ == "__main__":
-    main()
+    pass
