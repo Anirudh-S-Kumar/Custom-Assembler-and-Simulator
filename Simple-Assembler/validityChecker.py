1f09ce1e30f582ef2bed0fb63fb7cee85fed4399
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-08-08 19:43:27 +0530

    fixed error for integer in movf, general error

--- Simple-Assembler/validityChecker.py
+++ Simple-Assembler/validityChecker.py
@@ -2,10 +2,9 @@ from allConstants import *
 from assemblerHelpers import *
 
 
-
-def isValidInstr(inst: str, variables: list, memory:dict) -> tuple:
+def isValidInstr(inst: str, variables: list, memory: dict) -> tuple:
     """Return True if the instruction is a valid instruction, else returns false
-    
+
     Parameter
     -------
     inst : str
@@ -18,37 +17,39 @@ def isValidInstr(inst: str, variables: list, memory:dict) -> tuple:
     intcount = 0
     instToken = inst.split()
     validIns = isInstruction(instToken[0])
-    if not (validIns): # Returning false if Instruction is not a valid instruction
+    if not (validIns):  # Returning false if Instruction is not a valid instruction
         return False, "Instruction given is not a valid instruction"
 
     type = returnType(inst)
 
     type_struct = type_structure[type]
 
-    #iterating through all the elements of the instruction and making sure all are correct
+    # iterating through all the elements of the instruction and making sure all are correct
     i = 0
     for j in type_struct:
-        # try:
+        try:
             if j == "unused":
                 continue
 
             if j == "opcode":
-                i+=1    
+                i += 1
                 continue
 
             elif j == "memory":
                 if type == 'd':
                     if not inVars(instToken[i], variables):
-                        return False , f"'{instToken[i]}' variable not defined"
+                        return False, f"'{instToken[i]}' variable not defined"
                 elif type == "e":
                     if not inMemory(instToken[i], memory):
                         return False, f"'{instToken[i]}' label used is not defined"
-            
+
             elif j == "immediate":
                 if instToken[i][0] != "$":
                     return False, "Immediate value missing"
-                
+
                 num = instToken[i][1:]
+                num_copy = num
+                # print(num_copy.index('.'))
 
                 if not (isNumber(num)):
                     return False, "Not a valid number"
@@ -61,25 +62,31 @@ def isValidInstr(inst: str, variables: list, memory:dict) -> tuple:
                 if instToken[0] == "mov":
                     if not(getFractional(num) == 0):
                         return False, "Immediate value must be an integer"
-                
+
                 elif instToken[0] == 'movf':
+                    try:
+                        (num_copy.index('.'))
+                    except ValueError:
+                        return False, "Immediate value is not a floating point number"
+
                     if not validFloat(num):
                         return False, "Immediate value can't be represented as per the ISA specifications"
 
             elif j == "reg":
                 if not isRegister(instToken[i]):
                     return False, f"'{instToken[i]}' is not a Valid Register"
-                
+
                 if instToken[i] == 'FLAGS':
                     if (i != 1) or instToken[0] != "mov":
                         return False, "Illegal use of flag"
-            i+=1
-        # except:
-        #     return False, "Something went terribly wrong. Should check up on that"
+            i += 1
+        except:
+            return False, "General Syntax Error"
 
-    return True, "" # returning true with an empty string if all checks pass
+    return True, ""  # returning true with an empty string if all checks pass
 
-def weakIsLabel(inst:str, variables:list, memory:dict) -> tuple:
+
+def weakIsLabel(inst: str, variables: list, memory: dict) -> tuple:
     """A weaker version of the isValidLabel instruction"""
     inst = inst.split()
     if (inst[0][-1] != ":"):
@@ -120,12 +127,12 @@ def isValidLabel(inst: str, variables: list, memory: dict) -> tuple:
 #     """Return true if an instruction is valid at all or not"""
 #     pass
 
+
 def main():
     """For Testing only"""
-    variables = [{"abc": 10, "asdwe": 11, "asdawd":12}]
+    variables = [{"abc": 10, "asdwe": 11, "asdawd": 12}]
     memory = {'label': 7}
     print(isValidInstr("movf R2 $1.5", variables=variables, memory=memory))
-
 
 
 if __name__ == "__main__":
